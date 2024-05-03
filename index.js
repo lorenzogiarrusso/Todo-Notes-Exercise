ab25a1e6134328ae742742a0e70fdb9697e3dc08
lorenzogiarrusso <119191590+lorenzogiarrusso@users.noreply.github.com>
2024-05-03 08:45:52 +0200

    Add comments

--- index.js
+++ index.js
@@ -20,22 +20,24 @@ app.use(express.static(path.join(__dirname, "public")));
 const mongoDBUri = "mongodb+srv://giarrussolorenzo:t1otEqlgBECuv4NL@cluster0.hqzaedi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+//Port number used on local host
 const port = 3000;
 
 const todoSchema = new mongoose.Schema({
-    todoText: String,
-    todoDue: Date,
-    todoCompleted: { type: Boolean, default: false }
+    todoText: { type: String, required: true }, //To-do's description
+    todoDue: { type: Date, required: true }, //Date before which the to-do should be completed
+    todoCompleted: { type: Boolean, default: false } //Boolean indicating whether the to-do has been completed
 });
 
 const noteSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    todos: [todoSchema]
+    title: { type: String, required: true }, //Note title
+    description: String, //Note description
+    todos: [todoSchema] //Array of to-dos belonging to the note
 });
 
 const Note = mongoose.model("Note", noteSchema);
 
+//GET homepage
 app.get("/", async (req, res) => {
     try {
         const notes = await Note.find();
@@ -47,10 +49,12 @@ app.get("/", async (req, res) => {
     }
 });
 
+//GET webpage to create a new note
 app.get("/compose", (req, res) => {
     res.render("compose.ejs");
 });
 
+//POST received when the user creates a new note through the compose webpage
 app.post("/compose", async (req, res) => {
     let newNote = new Note({
         title: req.body.title,
@@ -75,7 +79,7 @@ app.post("/compose", async (req, res) => {
 
     try {
         await newNote.save();
-        res.redirect("/");
+        res.redirect("/"); //Redirect to homepage
     }
     catch (err) {
         console.error(err);
@@ -83,6 +87,7 @@ app.post("/compose", async (req, res) => {
     }
 });
 
+//Used for search bar functionality
 app.get('/search', async (req, res) => {
     if (!req.query.query) {
         return res.status(400).send('Query parameter is required');
@@ -100,6 +105,7 @@ app.get('/search', async (req, res) => {
     }
 });
 
+//GET webpage with details on a specific note based on its id
 app.get("/notes/:noteId", async (req, res) => {
     try {
         const note = await Note.findOne({ _id: req.params.noteId });
@@ -120,6 +126,7 @@ app.get("/notes/:noteId", async (req, res) => {
     }
 })
 
+//POST used to delete a note specified through its id. Used when the user click on "Delete note" in the /notes/:noteId webpage
 app.post("/notes/:noteId/delete", async (req, res) => {
     try {
         const note = await Note.findByIdAndDelete(req.params.noteId);
@@ -131,14 +138,15 @@ app.post("/notes/:noteId/delete", async (req, res) => {
     }
 });
 
+//POST used to add new todos to an existing node specified through its id. Used when the user submits new to-dos through the /notes/:noteId webpage
 app.post("/notes/:noteId/addTodos", async (req, res) => {
     try {
         const note = await Note.findById(req.params.noteId);
         if (req.body.todoText) {
-            if (typeof req.body.todoText == "string") { // If there is only one to-do, req.body.todoText will be a single string...
+            if (typeof req.body.todoText == "string") { // If there is only one to-do, req.body.todoText will be a single string, so we simply push it onto the note.todos array
                 note.todos.push({ todoText: req.body.todoText, todoDue: req.body.todoDue });
             }
-            else { // ...oherwise, it will be an array.
+            else { // ...oherwise, it will be an array. In this case, we iterate over it and push elements onto the note.todos array
                 const todoQty = req.body.todoText.length;
                 for (let i = 0; i < todoQty; i++) {
                     note.todos.push({
@@ -156,11 +164,13 @@ app.post("/notes/:noteId/addTodos", async (req, res) => {
     res.redirect('back');
 });
 
+//POST used to delete a specific to-do from an existing note. Used when the user clicks on a "Delete to-do" button in the /notes/noteId webpage
+//:index is the to-do's position in the note's todos array.
 app.post("/notes/:noteId/deleteTodo/:index", async (req, res) => {
     try {
         const note = await Note.findById(req.params.noteId);
 
-        note.todos.splice(req.params.index, 1);
+        note.todos.splice(req.params.index, 1); //remove to-do from the note.todos array based on its index in the array
 
         await note.save();
     } catch (err) {
@@ -170,6 +180,8 @@ app.post("/notes/:noteId/deleteTodo/:index", async (req, res) => {
     res.redirect('back');
 });
 
+//POST used to toggle the "completed" state of a to-do belonging to a note. Used when the user clicks on a to-do checkbox in the notes/:noteId webpage.
+//req.body.check contains the boolean value to be assigned to the to-do's todoCompleted field. The change is applied to the database.
 app.post("/notes/:noteId/toggleTodo/:index", async (req, res) => {
     try {
         const note = await Note.findById(req.params.noteId);
@@ -181,7 +193,7 @@ app.post("/notes/:noteId/toggleTodo/:index", async (req, res) => {
         console.error(err);
         res.status(500).send("Error toggling to-do in note");
     }
-    res.send(undefined);
+    res.send(undefined); //There is no need to send back anything as this function merely updates the database
 });
 
 app.listen(port, () => {
